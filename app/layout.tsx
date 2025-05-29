bf15484569b61d721a27df7b057d19b248aa7e65
Stolbov Nikita <nikita-stolbov-99@mail.ru>
2025-05-29 21:48:59 +0300

    Настроить шрифт и цвета

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const notoSans = Noto_Sans({
+  variable: "--font-noto-sans",
+  weight: ["300","400","500","600", "700"],
+  subsets: ["cyrillic"],
+  fallback: ["sans-serif"]
 });
 
 export const metadata: Metadata = {
@@ -23,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="ru">
+      <body className={`${notoSans.variable}`}>
         {children}
       </body>
     </html>
