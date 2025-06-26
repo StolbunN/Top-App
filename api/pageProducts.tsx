b9f36402bac2d1bc52f7478d0dbc4756ce714fcd
Stolbov Nikita <nikita-stolbov-99@mail.ru>
2025-06-26 20:28:49 +0300

    Добавить упрощенную версию loading.tsx

--- api/pageProducts.tsx
+++ api/pageProducts.tsx
@@ -2,6 +2,7 @@ import { API } from "@/app/api";
 import { TopPageModel } from "@/interfaces/page.interface";
 
 export async function getProducts(alias: string): Promise<TopPageModel | null> {
+  await new Promise(res => setTimeout(() => res(""), 3000));
   const res = await fetch(`${API.topPage.byAlias}/${alias}`, {
     next: { revalidate: 10 }
   });
