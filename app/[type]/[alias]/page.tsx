23ef9412eaf784a0169d4a41e2afc2886c9a8f99
Stolbov Nikita <nikita-stolbov-99@mail.ru>
2025-06-26 20:09:35 +0300

    Проработать логику роутинга по страницам из меню

--- app/[type]/[alias]/page.tsx
+++ app/[type]/[alias]/page.tsx
@@ -13,25 +13,41 @@ interface PageProductsProps {
   params: Promise<{ type: string; alias: string }>
 };
 
+
 export async function generateStaticParams() {
-  let menuItems: {type: string, alias: string}[] = [];
-  for(const firstLevelItem of firstLevelMenu) {
+  let menuItems: { type: string, alias: string }[] = [];
+  for (const firstLevelItem of firstLevelMenu) {
     const menu = await getMenu(firstLevelItem.id);
-    menuItems = menuItems.concat(menu.flatMap(item => item.pages.map(page => ({type: firstLevelItem.route, alias: page.alias}))));
+    menuItems = menuItems.concat(menu.flatMap(item => item.pages.map(page => ({ type: firstLevelItem.route, alias: page.alias }))));
   }
   return menuItems;
 }
 
 export default async function PageProducts({ params }: PageProductsProps) {
-  const { alias } = await params;
-  const page = await getProducts(alias);
-  if (!page) {
+  try {
+    const { type, alias } = await params;
+    const page = await getProducts(alias);
+    const category = page?.firstCategory;
+    const route = firstLevelMenu.find(menuItem => menuItem.id == category)?.route;
+
+    let paths: string[] = [];
+    if (!Number.isFinite(category) || category == undefined) {
+      notFound();
+    }
+    const menu = await getMenu(category);
+    paths = menu.flatMap(item => item.pages.map(page => `/${route}/${page.alias}`));
+
+    if (!paths.find(path => path == `/${type}/${alias}`)) {
+      notFound();
+    }
+    
+    return (
+      <div>
+        Страница с alias {alias}
+        <div>{page?.title}</div>
+      </div>
+    );
+  } catch {
     notFound();
   }
-  return (
-    <div>
-      Страница с alias {alias}
-      <div>{page.title}</div>
-    </div>
-  );
 }
